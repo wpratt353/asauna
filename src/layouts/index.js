79dedce83fdb7fa0d169cd743dc92847afab5891
willlaptop-PC\willlaptop <wpratt353@gmail.com>
2020-06-24 19:31:12 -0400

    sub footer first draft

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { ThemeProvider } from 'styled-components'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import SubFooter from '../components/Footer/SubFooter'
 import Footer from '../components/Footer'
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
@@ -11,6 +12,7 @@ const Layout = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
       <div className="siteContent">{children}</div>
+      <SubFooter />
       <Footer />
       <GlobalStyle />
     </ThemeProvider>
